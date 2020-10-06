7c75f1cc094c89c796596daaf05b86d975d5ecbf
purbayanto <romdoni.purbayanto@tiket.com>
2020-10-06 20:41:24 +0700

    binary search tree problems

--- src/main/java/id/dondon/binarytrees/binarysearchtree/BinarySearchTree.java
+++ src/main/java/id/dondon/binarytrees/binarysearchtree/BinarySearchTree.java
@@ -37,4 +37,92 @@ public class BinarySearchTree {
         }
     }
 
+    public int minimumValue(Node<Integer> head) {
+        if (head == null) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (head.getLeftChild() == null) {
+            return head.getData();
+        }
+
+        return minimumValue(head.getLeftChild());
+    }
+
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.getLeftChild() == null && root.getRightChild() == null) {
+            return 0;
+        }
+
+        int leftMaxDepth = 1 + maxDepth(root.getLeftChild());
+        int rightMaxDepth = 1 + maxDepth(root.getRightChild());
+
+        return Math.max(leftMaxDepth, rightMaxDepth);
+    }
+
+    public void mirror(Node<Integer> root) {
+        if (root == null) {
+            return;
+        }
+
+        mirror(root.getLeftChild());
+        mirror(root.getRightChild());
+
+        // Swap the left and the right child of each node.
+        Node<Integer> temp = root.getLeftChild();
+        root.setLeftChild(root.getRightChild());
+        root.setRightChild(temp);
+    }
+
+    public int countTrees(int numNodes) {
+        if (numNodes <= 1) {
+            return 1;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= numNodes; i++) {
+            int countLeftTrees = countTrees(i - 1);
+            int countRightTrees = countTrees(numNodes - i);
+
+            sum = sum + (countLeftTrees * countRightTrees);
+        }
+
+        return sum;
+    }
+
+    public void printRange(Node<Integer> root, int low, int high) {
+        if (root == null) {
+            return;
+        }
+
+        if (low <= root.getData()) {
+            printRange(root.getLeftChild(), low, high);
+        }
+
+        if (low <= root.getData() && root.getData() <= high) {
+            System.out.println(root.getData());
+        }
+
+        if (high > root.getData()) {
+            printRange(root.getRightChild(), low, high);
+        }
+    }
+
+    public boolean isBinarySearchTree(Node<Integer> root, int min, int max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.getData() <= min || root.getData() > max) {
+            return false;
+        }
+
+        return isBinarySearchTree(root.getLeftChild(), min, root.getData())
+            && isBinarySearchTree(root.getRightChild(), root.getData(), max);
+    }
+
 }
--- src/test/java/id/dondon/binarytrees/BinarySearchTreeTest.java
+++ src/test/java/id/dondon/binarytrees/BinarySearchTreeTest.java
@@ -2,6 +2,8 @@ package id.dondon.binarytrees;
 
 import id.dondon.binarytrees.binarysearchtree.BinarySearchTree;
 import id.dondon.binarytrees.depthfirsttravelsal.BinaryTreeInOrderTraversal;
+import id.dondon.queue.QueueOverflowException;
+import id.dondon.queue.QueueUnderflowException;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -9,6 +11,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class BinarySearchTreeTest {
 
@@ -72,6 +75,172 @@ public class BinarySearchTreeTest {
 
         assertEquals(Integer.valueOf(7), result7.getData());
         assertNull(result12);
+    }
+
+    @Test
+    public void minimumValue() {
+        Node<Integer> one = new Node<Integer>(1);
+        Node<Integer> two = new Node<Integer>(2);
+        Node<Integer> three = new Node<Integer>(3);
+        Node<Integer> four = new Node<Integer>(4);
+        Node<Integer> five = new Node<Integer>(5);
+        Node<Integer> six = new Node<Integer>(6);
+        Node<Integer> seven = new Node<Integer>(7);
+        Node<Integer> eight = new Node<Integer>(8);
+        Node<Integer> nine = new Node<Integer>(9);
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        Node<Integer> head = binarySearchTree.insert(null, five);
+
+        binarySearchTree.insert(head, one);
+        binarySearchTree.insert(head, seven);
+        binarySearchTree.insert(head, three);
+        binarySearchTree.insert(head, eight);
+        binarySearchTree.insert(head, two);
+        binarySearchTree.insert(head, nine);
+        binarySearchTree.insert(head, six);
+        binarySearchTree.insert(head, four);
+
+        int result = binarySearchTree.minimumValue(head);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void maxDepth() {
+        Node<Integer> one = new Node<Integer>(1);
+        Node<Integer> two = new Node<Integer>(2);
+        Node<Integer> three = new Node<Integer>(3);
+        Node<Integer> four = new Node<Integer>(4);
+        Node<Integer> five = new Node<Integer>(5);
+        Node<Integer> six = new Node<Integer>(6);
+        Node<Integer> seven = new Node<Integer>(7);
+        Node<Integer> eight = new Node<Integer>(8);
+        Node<Integer> nine = new Node<Integer>(9);
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        Node<Integer> head = binarySearchTree.insert(null, five);
+
+        binarySearchTree.insert(head, one);
+        binarySearchTree.insert(head, seven);
+        binarySearchTree.insert(head, three);
+        binarySearchTree.insert(head, eight);
+        binarySearchTree.insert(head, two);
+        binarySearchTree.insert(head, nine);
+        binarySearchTree.insert(head, six);
+        binarySearchTree.insert(head, four);
+
+        int result = binarySearchTree.maxDepth(head);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void mirror() throws QueueUnderflowException, QueueOverflowException {
+        Node<Integer> one = new Node<Integer>(1);
+        Node<Integer> two = new Node<Integer>(2);
+        Node<Integer> three = new Node<Integer>(3);
+        Node<Integer> four = new Node<Integer>(4);
+        Node<Integer> five = new Node<Integer>(5);
+        Node<Integer> six = new Node<Integer>(6);
+        Node<Integer> seven = new Node<Integer>(7);
+        Node<Integer> eight = new Node<Integer>(8);
+        Node<Integer> nine = new Node<Integer>(9);
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        Node<Integer> head = binarySearchTree.insert(null, five);
+
+        binarySearchTree.insert(head, one);
+        binarySearchTree.insert(head, seven);
+        binarySearchTree.insert(head, three);
+        binarySearchTree.insert(head, eight);
+        binarySearchTree.insert(head, two);
+        binarySearchTree.insert(head, nine);
+        binarySearchTree.insert(head, six);
+        binarySearchTree.insert(head, four);
+
+        System.out.println("Original");
+        BinaryTreeBreadthFirstTraversal binaryTreeBreadthFirstTraversal = new BinaryTreeBreadthFirstTraversal();
+        binaryTreeBreadthFirstTraversal.breadthFirst(head);
+        binarySearchTree.mirror(head);
+        System.out.println();
+        System.out.println("Mirror");
+        List<Object> result = binaryTreeBreadthFirstTraversal.breadthFirst(head);
+        List<Integer> expected = Arrays.asList(5, 7, 1, 8, 6, 3, 9, 4, 2);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void countTrees() {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        int result1 = binarySearchTree.countTrees(1);
+        assertEquals(1, result1);
+
+        int result2 = binarySearchTree.countTrees(2);
+        assertEquals(2, result2);
+
+        int result3 = binarySearchTree.countTrees(3);
+        assertEquals(5, result3);
+
+        int result6 = binarySearchTree.countTrees(6);
+        assertEquals(132, result6);
+    }
+
+    @Test
+    public void printRange() throws QueueUnderflowException, QueueOverflowException {
+        Node<Integer> one = new Node<Integer>(1);
+        Node<Integer> two = new Node<Integer>(2);
+        Node<Integer> three = new Node<Integer>(3);
+        Node<Integer> four = new Node<Integer>(4);
+        Node<Integer> five = new Node<Integer>(5);
+        Node<Integer> six = new Node<Integer>(6);
+        Node<Integer> seven = new Node<Integer>(7);
+        Node<Integer> eight = new Node<Integer>(8);
+        Node<Integer> nine = new Node<Integer>(9);
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        Node<Integer> head = binarySearchTree.insert(null, five);
+
+        binarySearchTree.insert(head, one);
+        binarySearchTree.insert(head, seven);
+        binarySearchTree.insert(head, three);
+        binarySearchTree.insert(head, eight);
+        binarySearchTree.insert(head, two);
+        binarySearchTree.insert(head, nine);
+        binarySearchTree.insert(head, six);
+        binarySearchTree.insert(head, four);
+
+        System.out.println("Range");
+        binarySearchTree.printRange(head, 2, 9);
+    }
+
+    @Test
+    public void isBinarySearchTree() {
+        Node<Integer> one = new Node<Integer>(1);
+        Node<Integer> two = new Node<Integer>(2);
+        Node<Integer> three = new Node<Integer>(3);
+        Node<Integer> four = new Node<Integer>(4);
+        Node<Integer> five = new Node<Integer>(5);
+        Node<Integer> six = new Node<Integer>(6);
+        Node<Integer> seven = new Node<Integer>(7);
+        Node<Integer> eight = new Node<Integer>(8);
+        Node<Integer> nine = new Node<Integer>(9);
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        Node<Integer> head = binarySearchTree.insert(null, five);
+
+        binarySearchTree.insert(head, one);
+        binarySearchTree.insert(head, seven);
+        binarySearchTree.insert(head, three);
+        binarySearchTree.insert(head, eight);
+        binarySearchTree.insert(head, two);
+        binarySearchTree.insert(head, nine);
+        binarySearchTree.insert(head, six);
+        binarySearchTree.insert(head, four);
+
+        boolean result = binarySearchTree.isBinarySearchTree(head, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        assertTrue(result);
     }
 
 }
