d6ef2db8ecb671345a28f08cf72f5c1676916dc3
purbayanto <romdoni.purbayanto@tiket.com>
2020-10-08 14:32:58 +0700

    min & max heap

---
+++ src/test/java/id/dondon/heaps/MaxHeapTest.java
@@ -0,0 +1,51 @@
+package id.dondon.heaps;
+
+import id.dondon.heaps.Heap.HeapEmptyException;
+import id.dondon.heaps.Heap.HeapFullException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxHeapTest {
+
+  @Test
+  public void insert() throws HeapFullException, HeapEmptyException {
+    MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(Integer.class);
+    maxHeap.insert(9);
+    maxHeap.insert(4);
+    maxHeap.insert(17);
+    maxHeap.printHeapArray();
+    maxHeap.insert(6);
+    maxHeap.printHeapArray();
+
+    int highestPriority1 = maxHeap.getHighestPriority();
+    Assert.assertEquals(17, highestPriority1);
+
+    maxHeap.insert(18);
+    maxHeap.printHeapArray();
+
+    int highestPriority2 = maxHeap.getHighestPriority();
+    Assert.assertEquals(18, highestPriority2);
+
+    maxHeap.insert(20);
+    maxHeap.printHeapArray();
+
+    int highestPriority3 = maxHeap.getHighestPriority();
+    Assert.assertEquals(20, highestPriority3);
+  }
+
+  @Test
+  public void removeHighestPriority() throws HeapFullException, HeapEmptyException {
+    MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(Integer.class);
+    maxHeap.insert(9);
+    maxHeap.insert(4);
+    maxHeap.insert(17);
+    maxHeap.insert(6);
+
+    int highestPriority1 = maxHeap.removeHighestPriority();
+    Assert.assertEquals(17, highestPriority1);
+
+    int highestPriority2 = maxHeap.getHighestPriority();
+    Assert.assertEquals(9, highestPriority2);
+  }
+
+}
---
+++ src/test/java/id/dondon/heaps/MinHeapTest.java
@@ -0,0 +1,51 @@
+package id.dondon.heaps;
+
+import id.dondon.heaps.Heap.HeapEmptyException;
+import id.dondon.heaps.Heap.HeapFullException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinHeapTest {
+
+  @Test
+  public void insert() throws HeapFullException, HeapEmptyException {
+    MinHeap<Integer> minHeap = new MinHeap<Integer>(Integer.class);
+    minHeap.insert(9);
+    minHeap.insert(4);
+    minHeap.insert(17);
+    minHeap.printHeapArray();
+    minHeap.insert(6);
+    minHeap.printHeapArray();
+
+    int highestPriority1 = minHeap.getHighestPriority();
+    Assert.assertEquals(4, highestPriority1);
+
+    minHeap.insert(2);
+    minHeap.printHeapArray();
+
+    int highestPriority2 = minHeap.getHighestPriority();
+    Assert.assertEquals(2, highestPriority2);
+
+    minHeap.insert(1);
+    minHeap.printHeapArray();
+
+    int highestPriority3 = minHeap.getHighestPriority();
+    Assert.assertEquals(1, highestPriority3);
+  }
+
+  @Test
+  public void removeHighestPriority() throws HeapFullException, HeapEmptyException {
+    MinHeap<Integer> minHeap = new MinHeap<Integer>(Integer.class);
+    minHeap.insert(9);
+    minHeap.insert(4);
+    minHeap.insert(17);
+    minHeap.insert(6);
+
+    int highestPriority1 = minHeap.removeHighestPriority();
+    Assert.assertEquals(4, highestPriority1);
+
+    int highestPriority2 = minHeap.getHighestPriority();
+    Assert.assertEquals(6, highestPriority2);
+  }
+
+}
