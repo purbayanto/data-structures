3f2cb532118fced60c96246f3f77999c75dae384
purbayanto <romdoni.purbayanto@tiket.com>
2020-10-31 18:56:30 +0700

    add fixme note

--- src/main/java/id/dondon/ctci/chapter1/InterviewQuestion3.java
+++ src/main/java/id/dondon/ctci/chapter1/InterviewQuestion3.java
@@ -1,8 +1,5 @@
 package id.dondon.ctci.chapter1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Write a method to replace all spaces in a string with %20
  * You may assume that the string has sufficient space at the end to hold the additional characters,
